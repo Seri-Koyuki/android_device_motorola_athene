e2c5bfa0d9b33de6c4b6ef21b24e4d1c6953ae13
Seri Koyuki <64983260+Seri-Koyuki@users.noreply.github.com>
2020-07-06 16:38:46 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -13,12 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
-# Device was launched with MM
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Also get non-open-source specific aspects if available
 $(call inherit-product, vendor/motorola/athene/athene-vendor.mk)
